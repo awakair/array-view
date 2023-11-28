7f96b8ef206fe9cd892d317e3e36909e95c7c910
Dmitry Sukhov <dm1tryceg@yandex.ru>
2023-11-28 18:25:43 +0300

    Added array_n_view.h

--- bin/main.cpp
+++ bin/main.cpp
@@ -2,18 +2,38 @@
 #include <cstdint>
 #include <lib/uint17_view.h>
 #include <lib/array.h>
+#include <lib/array_n_view.h>
 
  using namespace uint17;
 
+ template <typename T>
+ void foo(T) = delete; 
+
 int main() {
   Array some(5);
-  uint16_t some_val;
-  std::cin >> some_val;
-  std::cout << some_val << std::endl;
+  uint32_t some_val = 20;
+//  std::cin >> some_val;
+//  std::cout << some_val << std::endl;
   some[0] = some_val;
   some[1] = ++some_val;
   some[2] = ++some_val;
-  std::cout << some[0] << " " << some[1] << " " << some[2];
-  
+  some[3] = ++some_val;
+  some[4] = ++some_val;
+//  ArrayView<1> view(some, 1, 4);
+
+  size_t width = 2;
+  size_t height = 2;
+  ArrayView<2> view2d(some, 1, width, height);
+  view2d[0][0] = 5u;
+  view2d[0][1] = 6u;
+  view2d[0].Get(0) = 9u;
+  view2d[1][0] = 7u;
+  view2d[1][1] = 8u;
+  view2d.Get(--width, --height) = 10u;
+
+
+  for (size_t i = 0; i != 5; ++i) {
+    std::cout << some[i] << " ";
+  }
   return EXIT_SUCCESS;
 }
