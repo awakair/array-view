c9b292f3a43d7fc6c62b2ee68576dd04cf9188d1
Dmitry Sukhov <dm1tryceg@yandex.ru>
2023-11-30 11:36:21 +0300

    Added utils.h and rescristions.png

---
+++ src/uint17/utils.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <utility>
+
+
+
+namespace uint17::utils {
+
+template <typename T>
+void Swap(T& a, T& b) { // I am not allowed to use std::swap
+  T tmp = std::move(a);
+  a = std::move(b);
+  b = std::move(tmp);
+}
+
+} // namespace uint17::utils
